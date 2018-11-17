2853b0b6ff9b8e873a8a2240b35ee647e9234443
Ian Clarkson <clarksonian@gmail.com>
2018-11-17 13:10:59 -0800

    Add names for contract styles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_17_035909) do
+ActiveRecord::Schema.define(version: 2018_11_17_210851) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2018_11_17_035909) do
     t.string "lettercode"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   create_table "departments", force: :cascade do |t|
