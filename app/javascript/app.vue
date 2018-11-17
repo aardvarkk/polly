eda8a1c8f05edf9b7033b9472fba809db7f1dd30
Andrew Kalek <andrew.kalek@anlek.com>
2018-11-16 18:14:39 -0800

    New layout

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,19 +1,83 @@
 <template>
-  <div id="app">
-    <p>{{ message }}</p>
+<div id="app">
+  <div class="wrapper">
+    <div class="sidebar" data-color="blue">
+      <div class="logo">
+        <a href="http://www.creative-tim.com" class="simple-text pl-2 logo-normal">
+          Polly Data
+        </a>
+        <div class="navbar-minimize">
+          <button id="minimizeSidebar" class="btn btn-simple btn-icon btn-neutral btn-round">
+          <i class="now-ui-icons text_align-center visible-on-sidebar-regular"></i>
+          <i class="now-ui-icons design_bullet-list-67 visible-on-sidebar-mini"></i>
+          </button>
+        </div>
+      </div>
+      <div class="sidebar-wrapper">
+        <ul class="nav">
+          <li class="active">
+            <a href="#">
+              <i class="now-ui-icons design_app"></i>
+              <p>Dashboard</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="main-panel">
+      <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute">
+        <div class="container-fluid">
+          <div class="navbar-wrapper">
+            <div class="navbar-toggle">
+              <button type="button" class="navbar-toggler">
+              <span class="navbar-toggler-bar bar1"></span>
+              <span class="navbar-toggler-bar bar2"></span>
+              <span class="navbar-toggler-bar bar3"></span>
+              </button>
+            </div>
+            <a class="navbar-brand" href="#pablo">Dashboard</a>
+          </div>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-bar navbar-kebab"></span>
+            <span class="navbar-toggler-bar navbar-kebab"></span>
+            <span class="navbar-toggler-bar navbar-kebab"></span>
+          </button>
+          <div class="collapse navbar-collapse justify-content-end" id="navigation">
+            <form>
+              <div class="input-group no-border">
+                <input type="text" value="" class="form-control" placeholder="Search...">
+                <span class="input-group-append">
+                  <span class="input-group-text">
+                    <i class="now-ui-icons ui-1_zoom-bold"></i>
+                  </span>
+                </span>
+              </div>
+            </form>
+          </div>
+        </div>
+      </nav>
+      <div class="panel-header panel-header-sm"></div>
+      <div class="content">
+        <div class="card">
+          <div class="card-body">
+            {{message}}
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
+</div>
 </template>
 
 <script>
-export default {
-  data: function () {
-    return {
-      message: "Hello Polly!!!"
+  export default {
+    data: function () {
+      return {
+        message: "Hello Polly from Vue!!!"
+      }
     }
   }
-}
 </script>
 
 <style scoped>
-
 </style>
