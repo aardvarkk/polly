3198922cb8155369080520673cf53f6196da66a9
Andrew Kalek <andrew.kalek@anlek.com>
2018-11-16 20:18:39 -0800

    Added router and dashboard

--- app/javascript/packs/entry_point.js
+++ app/javascript/packs/entry_point.js
@@ -1,11 +1,21 @@
 // import Vue from 'vue'
 import Vue from 'vue/dist/vue.esm'
 
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+import routes from '../routes'
+const router = new VueRouter({
+  mode: 'history',
+  routes // short for `routes: routes`
+})
 import App from '../app.vue'
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
     el: '#app',
-    render: h => h(App)
+    render: h => h(App),
+    router
   })
 })
---
+++ app/javascript/routes.js
@@ -0,0 +1,7 @@
+import Dashboard from 'pages/dashboard.vue'
+
+const routes = [
+  { path: '/dashboard', component: Dashboard },
+]
+
+export default routes
