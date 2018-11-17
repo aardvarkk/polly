748c9b31a507d508a44cdeee8dc89578e8faf103
milbz87 <alex.burn1987@gmail.com>
2018-11-17 14:17:31 -0800

    fixed mounted issues

--- app/javascript/components/purchases-category-stacked-chart.js
+++ app/javascript/components/purchases-category-stacked-chart.js
@@ -21,81 +21,86 @@ export default {
             default () { return {} },
         }
     },
-    mounted() {
-        // Overwriting base render method with actual data.
-        const newFilter = clone(this.filter);
-        newFilter.group_by = 'category_id,yukon'
+    methods: {
+        rerender() {
+            const newFilter = clone(this.filter);
+            newFilter.group_by = 'category_id,yukon'
 
-        DataApi.fetch('categories').then(categories => {
-            console.log(categories)
+            DataApi.fetch('categories').then(categories => {
+                console.log(categories)
 
-            PurchaseApi.aggregate('sum', newFilter).then(result => {
-                const inYukonData = {}
-                const outYukonData = {}
+                PurchaseApi.aggregate('sum', newFilter).then(result => {
+                    const inYukonData = {}
+                    const outYukonData = {}
 
-                result.forEach(group => {
-                    if (group.yukon === true) {
-                        inYukonData[group.category_id] = group.sum
-                    } else {
-                        outYukonData[group.category_id] = group.sum
-                    }
-                })
-
-                let categoryKeys = [];
-                Object.keys(inYukonData).forEach(k => {
-                    categories.forEach(x => {
-                        if (k == x.id) {
-                            categoryKeys.push(x.name)
+                    result.forEach(group => {
+                        if (group.yukon === true) {
+                            inYukonData[group.category_id] = group.sum
+                        } else {
+                            outYukonData[group.category_id] = group.sum
                         }
                     })
-                });
 
-                this.renderChart({
-                    labels: categoryKeys,
-                    datasets: [{
-                            label: 'In Yukon',
-                            backgroundColor: '#ff846e',
-                            data: Object.values(inYukonData).map(x => x / 100)
+                    let categoryKeys = [];
+                    Object.keys(inYukonData).forEach(k => {
+                        categories.forEach(x => {
+                            if (k == x.id) {
+                                categoryKeys.push(x.name)
+                            }
+                        })
+                    });
+
+                    this.renderChart({
+                        labels: categoryKeys,
+                        datasets: [{
+                                label: 'In Yukon',
+                                backgroundColor: '#ff846e',
+                                data: Object.values(inYukonData).map(x => x / 100)
+                            },
+                            {
+                                label: 'Out of Yukon',
+                                backgroundColor: '#70d5db',
+                                data: Object.values(outYukonData).map(x => x / 100)
+                            }
+                        ]
+                    }, {
+                        title: {
+                            display: true,
+                            text: "Spending by Category 2007 - 2018"
                         },
-                        {
-                            label: 'Out of Yukon',
-                            backgroundColor: '#70d5db',
-                            data: Object.values(outYukonData).map(x => x / 100)
-                        }
-                    ]
-                }, {
-                    title: {
-                        display: true,
-                        text: "Spending by Category 2007 - 2018"
-                    },
-                    scales: {
-                        yAxes: [{
-                            stacked: true,
-                            ticks: {
-                                beginAtZero: true,
-                                callback: function(value, index, values) {
-                                    return formatCurrency(value);
-                                },
+                        scales: {
+                            yAxes: [{
+                                stacked: true,
+                                ticks: {
+                                    beginAtZero: true,
+                                    callback: function(value, index, values) {
+                                        return formatCurrency(value);
+                                    },
 
-                            }
-                        }],
-                        xAxes: [{
-                            stacked: true,
-                            ticks: {
-                                autoSkip: false
-                            }
-                        }]
-                    },
-                    tooltips: {
-                        callbacks: {
-                            label: function(tooltipItem, data) {
-                                return formatCurrency(tooltipItem.yLabel);
+                                }
+                            }],
+                            xAxes: [{
+                                stacked: true,
+                                ticks: {
+                                    autoSkip: false
+                                }
+                            }]
+                        },
+                        tooltips: {
+                            callbacks: {
+                                label: function(tooltipItem, data) {
+                                    return formatCurrency(tooltipItem.yLabel);
+                                }
                             }
                         }
-                    }
+                    })
                 })
-            })
-        });
+            });
+        }
+    },
+    mounted() {
+        // Overwriting base render method with actual data.
+        this.rerender()
 
 
     }
--- app/javascript/components/purchases-department-stacked-chart.js
+++ app/javascript/components/purchases-department-stacked-chart.js
@@ -21,82 +21,85 @@ export default {
             default () { return {} },
         }
     },
-    mounted() {
-        // Overwriting base render method with actual data.
-        const newFilter = clone(this.filter);
-        newFilter.group_by = 'department_id,yukon'
+    methods: {
+        rerender() {
+            // Overwriting base render method with actual data.
+            const newFilter = clone(this.filter);
+            newFilter.group_by = 'department_id,yukon'
 
-        DataApi.fetch('departments').then(departments => {
-            console.log(departments)
+            DataApi.fetch('departments').then(departments => {
+                console.log(departments)
 
-            PurchaseApi.aggregate('sum', newFilter).then(result => {
-                const inYukonData = {}
-                const outYukonData = {}
+                PurchaseApi.aggregate('sum', newFilter).then(result => {
+                    const inYukonData = {}
+                    const outYukonData = {}
 
-                result.forEach(group => {
-                    if (group.yukon === true) {
-                        inYukonData[group.department_id] = group.sum
-                    } else {
-                        outYukonData[group.department_id] = group.sum
-                    }
-                })
-
-                let departmentKeys = [];
-                Object.keys(inYukonData).forEach(k => {
-                    departments.forEach(x => {
-                        if (k == x.id) {
-                            departmentKeys.push(x.name)
+                    result.forEach(group => {
+                        if (group.yukon === true) {
+                            inYukonData[group.department_id] = group.sum
+                        } else {
+                            outYukonData[group.department_id] = group.sum
                         }
                     })
-                });
 
-                this.renderChart({
-                    labels: departmentKeys,
-                    datasets: [{
-                            label: 'In Yukon',
-                            backgroundColor: '#ff846e',
-                            data: Object.values(inYukonData).map(x => x / 100)
+                    let departmentKeys = [];
+                    Object.keys(inYukonData).forEach(k => {
+                        departments.forEach(x => {
+                            if (k == x.id) {
+                                departmentKeys.push(x.name)
+                            }
+                        })
+                    });
+
+                    this.renderChart({
+                        labels: departmentKeys,
+                        datasets: [{
+                                label: 'In Yukon',
+                                backgroundColor: '#ff846e',
+                                data: Object.values(inYukonData).map(x => x / 100)
+                            },
+                            {
+                                label: 'Out of Yukon',
+                                backgroundColor: '#70d5db',
+                                data: Object.values(outYukonData).map(x => x / 100)
+                            }
+                        ]
+                    }, {
+                        title: {
+                            display: true,
+                            text: "Spending by Department 2007 - 2018"
                         },
-                        {
-                            label: 'Out of Yukon',
-                            backgroundColor: '#70d5db',
-                            data: Object.values(outYukonData).map(x => x / 100)
-                        }
-                    ]
-                }, {
-                    title: {
-                        display: true,
-                        text: "Spending by Department 2007 - 2018"
-                    },
-                    scales: {
-                        yAxes: [{
-                            stacked: true,
-                            ticks: {
-                                beginAtZero: true,
-                                callback: function(value, index, values) {
-                                    return formatCurrency(value);
-                                },
+                        scales: {
+                            yAxes: [{
+                                stacked: true,
+                                ticks: {
+                                    beginAtZero: true,
+                                    callback: function(value, index, values) {
+                                        return formatCurrency(value);
+                                    },
 
-                            }
-                        }],
-                        xAxes: [{
-                            stacked: true,
-                            ticks: {
-                                autoSkip: false
-                            }
-                        }]
-                    },
-                    tooltips: {
-                        callbacks: {
-                            label: function(tooltipItem, data) {
-                                return formatCurrency(tooltipItem.yLabel);
+                                }
+                            }],
+                            xAxes: [{
+                                stacked: true,
+                                ticks: {
+                                    autoSkip: false
+                                }
+                            }]
+                        },
+                        tooltips: {
+                            callbacks: {
+                                label: function(tooltipItem, data) {
+                                    return formatCurrency(tooltipItem.yLabel);
+                                }
                             }
                         }
-                    }
+                    })
                 })
-            })
-        });
-
-
+            });
+        }
+    },
+    mounted() {
+        this.rerender()
     }
 }
